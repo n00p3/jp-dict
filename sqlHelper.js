9b49659526023ec2161321b464d51f6f9563319a
Mateusz Głowski <no.name.dev.0@gmail.com>
2020-09-06 19:09:19 +0200

    Use new columns in query

--- sqlHelper.js
+++ sqlHelper.js
@@ -1,22 +1,29 @@
 const jmdict = require('better-sqlite3')('./dict/JMDict.sqlite3');
+const japanese = require('@lazy-cjk/japanese');
 
-module.exports.jmdict = async (word) => {
+function query(word) {
   // language=sql
   const stmt = jmdict.prepare(`
     select id, kanji, reading, gloss, position
     from entry
-    where kanji = ?                      -- Exact,
-       or kanji like '%, ' || ?          -- ends with,
-       or kanji like ?     || ',%'       -- starts with,
-       or kanji like '%, ' || ? || ', %' -- in the middle.
-       or reading = ?                     
-       or reading like '%, ' || ?      
-       or reading like ?     || ',%'   
-       or reading like '%, ' || ? || ', %' 
+    where kanji_always_hiragana = ?                      -- Exact,
+       or kanji_always_hiragana like '%, ' || ?          -- ends with,
+       or kanji_always_hiragana like ?     || ',%'       -- starts with,
+       or kanji_always_hiragana like '%, ' || ? || ', %' -- in the middle.
+       or reading_always_hiragana = ?                     
+       or reading_always_hiragana like '%, ' || ?      
+       or reading_always_hiragana like ?     || ',%'   
+       or reading_always_hiragana like '%, ' || ? || ', %' 
     limit 10;
   `)
-
   const rows = stmt.all(word, word, word, word, word, word, word, word);
+
+  return rows;
+}
+
+module.exports.jmdict = async (word) => {
+  const hiraganized = japanese.hiraganize(word);
+  const rows = query(hiraganized);
 
   return rows;
 }
