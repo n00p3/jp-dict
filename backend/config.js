bd21581c537f942f0e8c64857f6575c1d8b0b64a
Mateusz Głowski <no.name.dev.0@gmail.com>
2020-09-09 19:29:29 +0200

    Add text length validation

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  maxTextLength: 200,
+}
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const ve = require('@n00p3/ve-js');
 const words = require('./features/words').words;
 const extractKanji = require('./features/kanji').extractKanji;
 const sqlHelper = require('./sqlHelper');
+const {check, validationResult} = require('express-validator');
+const config = require('./config')
 
 const app = new express();
 const limiter = rateLimit({
@@ -19,27 +21,32 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/api/translate', async (req, res) => {
-
-  const result = {};
-  const text = req.query.text;
-  const features = req.query.features.replace(/\s/g,'').split(',');
-
-  result.words = await words(text);
-
-  if (features.includes('jmdict')) {
-    for (const word of result.words) {
-      const sql = await sqlHelper.jmdict(word.lemma === '*' ? word.word : word.lemma);
-      word.jmdict = sql;
+app.get('/api/translate',
+  [check('text').isLength({max: config.maxTextLength})
+    .withMessage(`Text cannot be longer than ${config.maxTextLength} characters.`)],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
     }
-  }
 
-  if (features.includes('kanji'))
-    result.kanji = await extractKanji(text);
+    const result = {};
+    const text = req.query.text;
+    const features = req.query.features.replace(/\s/g, '').split(',');
 
-  res.send(result);
+    result.words = await words(text);
 
-  // res.send({stat: 'ok'})
-});
+    if (features.includes('jmdict')) {
+      for (const word of result.words) {
+        const sql = await sqlHelper.jmdict(word.lemma === '*' ? word.word : word.lemma);
+        word.jmdict = sql;
+      }
+    }
+
+    if (features.includes('kanji'))
+      result.kanji = await extractKanji(text);
+
+    res.send(result);
+  });
 
 app.listen(3010)
