86c0b9d308fc5ff6a4155ef4fd047b88c9bfe557
Mateusz Głowski <no.name.dev.0@gmail.com>
2020-09-09 20:13:43 +0200

    Add footer and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Word from "./Word";
 import Kanji from "./Kanji";
 import * as _ from 'lodash';
 import RestHelper from "./RestHelper";
+import Navbar from "react-bootstrap/Navbar";
 
 class App extends React.Component {
   state = {
@@ -39,58 +40,84 @@ class App extends React.Component {
 
   render() {
     return (
-      <Container fluid='lg'>
-        <Row style={{marginTop: '5em'}}>
-          <Form style={{width: '100%'}}>
-            <Form.Group>
-              <Form.Control
-                type='input'
-                maxLength={200}
-                placeholder='Search anything...'
-                onChange={this.searchInputChangedDebounced}
-                ref={this.state.searchInputRef}
-              />
-            </Form.Group>
-          </Form>
-        </Row>
-        <Row>
-          {/*TODO: Tokenized sentence here*/}
-        </Row>
-        <Row>
-          <Col lg={8}>
-            <h4 className='dict-header'>
-              Words
-            </h4>
-            {
-              this.state.words.map((word, i) => {
-                return <React.Fragment key={'fragment-word-' + i}>
-                  <Word
-                    word={word}
-                    key={'word-' + i}
-                  />
-                  <hr key={'hr-word-' + i}/>
-                </React.Fragment>
-              })
-            }
-          </Col>
-          <Col lg={4}>
-            <h4 className='dict-header'>
-              Kanji
-            </h4>
-            {
-              this.state.kanji.map((kanji, i) => {
-                return <React.Fragment key={'fragment-kanji-' + i}>
-                  <Kanji
-                    kanji={kanji}
-                    key={'kanji-' + i}
-                  />
-                  <hr key={'hr-kanji-' + i}/>
-                </React.Fragment>
-              })
-            }
-          </Col>
-        </Row>
-      </Container>
+      <>
+        <Container fluid='lg'>
+          <Row>
+            <h1>Japanese Dictionary</h1>
+          </Row>
+          <Row>
+            <Form style={{width: '100%'}}>
+              <Form.Group>
+                <Form.Control
+                  type='input'
+                  maxLength={200}
+                  placeholder='Search anything...'
+                  onChange={this.searchInputChangedDebounced}
+                  ref={this.state.searchInputRef}
+                />
+              </Form.Group>
+            </Form>
+          </Row>
+          <Row>
+            {/*TODO: Tokenized sentence here*/}
+          </Row>
+          <Row>
+            <Col lg={8}>
+              <h4 className='dict-header'>
+                Words
+              </h4>
+              {
+                this.state.words.map((word, i) => {
+                  return <React.Fragment key={'fragment-word-' + i}>
+                    <Word
+                      word={word}
+                      key={'word-' + i}
+                    />
+                    <hr key={'hr-word-' + i}/>
+                  </React.Fragment>
+                })
+              }
+            </Col>
+            <Col lg={4}>
+              <h4 className='dict-header'>
+                Kanji
+              </h4>
+              {
+                this.state.kanji.map((kanji, i) => {
+                  return <React.Fragment key={'fragment-kanji-' + i}>
+                    <Kanji
+                      kanji={kanji}
+                      key={'kanji-' + i}
+                    />
+                    <hr key={'hr-kanji-' + i}/>
+                  </React.Fragment>
+                })
+              }
+            </Col>
+          </Row>
+        </Container>
+        <div style={{height: '3em'}}>
+
+        </div>
+        <footer>
+          <div className="fixed-bottom">
+            <Navbar color="dark" dark>
+              <Container>
+                <Row style={{margin: 'auto'}}>
+                  <Col md='auto'>
+                    <p>This project uses JMDict and KANJIDIC which are under <a href='#' target='blank'>
+                      Creative Commons Attribution-ShareAlike Licence (V3.0)
+                      <br/>
+                    </a> For more information check <a href='https://github.com/n00p3/jp-dict' target='blank'>project's
+                      repository</a>
+                    </p>
+                  </Col>
+                </Row>
+              </Container>
+            </Navbar>
+          </div>
+        </footer>
+      </>
     );
   }
 }
