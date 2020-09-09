bfea8ae92189349c67c1ed908020ef1d39e65e7a
Mateusz Głowski <no.name.dev.0@gmail.com>
2020-09-09 18:52:05 +0200

    Make kanji results unique

--- backend/features/kanji.js
+++ backend/features/kanji.js
@@ -1,12 +1,14 @@
 const hepburn = require('hepburn');
 const sqlHelper = require('../sqlHelper');
+const _ = require('lodash');
 
 module.exports.extractKanji = async (sentence) => {
-  const kanji = [];
+  let kanji = [];
   for (const char of sentence) {
     if (hepburn.containsKanji(char))
       kanji.push(await sqlHelper.kanjiDefinition(char));
   }
 
+  kanji = _.uniqBy(kanji, n => JSON.stringify(n));
   return kanji;
 }
